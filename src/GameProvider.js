83e30f119afda5e4c14393cfadbd626ed4858c97
joshua360x <jkw5499@psu.edu>
2022-02-24 13:59:25 -0500

    set up game provider for context

---
+++ src/GameProvider.js
@@ -0,0 +1,70 @@
+import { createContext, useContext, useState } from 'react';
+import initialCards from './cards-data';
+
+
+const CardGameContext = createContext();
+
+export default function GameProvider({ children }) {
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+  function findCardIndex(value, suit, cards) {
+    return cards.findIndex((card) => card.value === value && card.suit === suit);
+  }
+
+  function passCard(card) {
+    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
+    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
+
+    // arrays start at zero, but our players start at 1 :shrug:
+    const toHand = playerHands[to - 1] || deck;
+    const fromHand = playerHands[from - 1] || deck;
+
+    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
+    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
+
+    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
+    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
+
+    toHand.push(cardToMove);
+
+    toSetFunction([...toHand]);
+    fromSetFunction([...fromHand]);
+
+    setSelectedCard(null);
+  }
+
+  const gameStateAndSettersForCards = {
+    deck,
+    setDeck,
+    playerOneHand,
+    setPlayerOneHand,
+    selectedCard,
+    setSelectedCard,
+    playerTwoHand,
+    setPlayerTwoHand,
+    playerThreeHand,
+    setPlayerThreeHand,
+    from,
+    to,
+    setFrom,
+    setTo,
+    passCard,
+    findCardIndex,
+  };
+
+  return (
+    // <CardGameContext.Provider value={gameStateAndSettersForCards}>
+    //   {children}
+    // </CardGameContext.Provider>
+  );
+}
+
+export function useGameContext() {
+  return useContext(CardGameContext);
+}
