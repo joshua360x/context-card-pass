a7987bc9bd5b73fa2b3aa05ddabdc3099fe6da25
joshua360x <jkw5499@psu.edu>
2022-02-24 14:08:35 -0500

    ran test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import GameProvider from './GameProvider';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <GameProvider>
+      <App />
+    </GameProvider>
+  );
+  const linkElement = screen.getByText(/player 1/i);
   expect(linkElement).toBeInTheDocument();
 });
